25ac96b442e359bc765b845bff5732a834647141
clarkk <clark@electrobeat.dk>
2020-11-25 00:21:45 +0100

    update

---
+++ Cmd.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace cmdutils;
+
+class Cmd {
+	private $output 		= '';
+	
+	private $is_childproc 	= false;
+	private $proc;
+	private $pipes 			= [];
+	
+	private $pid;
+	private $exitcode 		= -1;
+	private $termsig 		= false;
+	
+	const PIPE_STDIN 		= 0;
+	const PIPE_STDOUT 		= 1;
+	const PIPE_STDERR 		= 2;
+	
+	public function __construct(bool $is_childproc=false){
+		$this->is_childproc = $is_childproc;
+	}
+	
+	public function output(): string{
+		return $this->output;
+	}
+	
+	public function is_success(): bool{
+		return !$this->exitcode;
+	}
+	
+	public function is_terminated(): bool{
+		return $this->termsig;
+	}
+	
+	public function get_pid(): int{
+		return $this->pid ?: proc_get_status($this->proc)['pid'];
+	}
+	
+	public function get_exitcode(): int{
+		return $this->exitcode;
+	}
+	
+	public function is_running(): bool{
+		$status 	= proc_get_status($this->proc);
+		$this->pid 	= $status['pid'];
+		
+		if($status['running']){
+			return true;
+		}
+		else{
+			$this->termsig = $status['termsig'] == 9;
+			
+			if($this->exitcode < 0){
+				$this->exitcode = $status['exitcode'];
+			}
+			
+			return false;
+		}
+	}
+	
+	public function get_pipe_stream(int $pipe){
+		$output = '';
+		$handle = $this->pipes[$pipe];
+		
+		$arr = [$handle];
+		$lulz1 = [];
+		$lulz2 = [];
+		
+		while(true){
+			if(stream_select($arr, $lulz1, $lulz2, 0, 100000) < 1){
+				break;
+			}
+			
+			$time_start = microtime(true);
+			$new 		= stream_get_contents($handle, 1);
+			$time_used 	= microtime(true) - $time_start;
+			
+			if(!is_string($new) || !strlen($new)){
+				break;
+			}
+			
+			$output .= $new;
+			
+			if($time_used > 0.1){
+				break;
+			}
+		}
+		
+		return $output;
+	}
+	
+	public function exec(string $syntax){
+		$this->proc = proc_open($syntax, [
+			self::PIPE_STDIN 	=> ['pipe', 'r'],
+			self::PIPE_STDOUT 	=> ['pipe', 'w'],
+			self::PIPE_STDERR 	=> ['pipe', 'w']
+		], $this->pipes);
+		fclose($this->pipes[0]);
+		
+		if($this->is_childproc){
+			stream_set_read_buffer($this->pipes[self::PIPE_STDOUT], 0);
+			stream_set_read_buffer($this->pipes[self::PIPE_STDERR], 0);
+			
+			stream_set_blocking($this->pipes[self::PIPE_STDOUT], false);
+			stream_set_blocking($this->pipes[self::PIPE_STDERR], false);
+		}
+		else{
+			$this->output 	= stream_get_contents($this->pipes[self::PIPE_STDOUT]);
+			$stderr 		= stream_get_contents($this->pipes[self::PIPE_STDERR]);
+			$this->close();
+			
+			return $stderr;
+		}
+	}
+	
+	public function close(){
+		foreach($this->pipes as $pipe){
+			if(is_resource($pipe)){
+				fclose($pipe);
+			}
+		}
+		
+		$this->exitcode = proc_close($this->proc);
+	}
+}

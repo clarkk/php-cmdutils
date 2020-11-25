6d2767a1752663d34195b030fca1a51807d2fbb0
clarkk <clark@electrobeat.dk>
2020-11-25 23:46:05 +0100

    update

---
+++ Procs_queue.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Utils\Procs_queue;
+
+class Procs_queue {
+	public function __construct(){
+		
+	}
+}
+
+class Error extends \Error {}
---
+++ procs_queue/test/test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../../Procs_queue.php';
+
+try{
+	$Queue = new \Utils\Procs_queue\Procs_queue;
+	
+	//$Net = new \Utils\Net\Net;
+	//$Net->request('https://www.google.com');
+	
+	/*$Test = new \Utils\Cmd\Test\Unit;
+	$Test->proc('echo this-test', '', 'this-test');
+	$Test->proc_stream('php procs/proc.php', 'An error occurred!', "First message\nSecond message\nThird message\nFinal message");
+	$Test->proc_stream('php procs/proc_exit.php', 'An fatal error occurred!', 'First message', 1);*/
+}
+catch(\Utils\Procs_queue\Error $e){
+	echo "\n\tTEST FAILED ON URL: ".$e->getMessage()."\n\n";
+}

182b29d59d00382e8a2be1f62ef52d370e26654b
clarkk <clark@electrobeat.dk>
2020-11-26 00:01:48 +0100

    update

--- Procs_queue.php
+++ Procs_queue.php
@@ -3,9 +3,14 @@
 namespace Utils\Procs_queue;
 
 class Procs_queue {
+	private $procs = [];
+	
 	public function __construct(){
-		
+		$nproc = (int)shell_exec('nproc');
+		echo "nproc: $nproc\n";
 	}
+	
+	
 }
 
 class Error extends \Error {}

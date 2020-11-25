88578ceedc192b1e3406b8eec21ed299d4e6bcdf
clarkk <clark@electrobeat.dk>
2020-11-25 02:14:38 +0100

    update

--- test/test.php
+++ test/test.php
@@ -1,72 +1,14 @@
 <?php
 
 require_once '../Cmd.php';
+require_once 'unit_test/Test.php';
 
-$cmd = new \Cmdutils\Cmd;
-$err = $cmd->exec('hostname');
-
-echo "ERROR: $err\n";
-echo 'Output: '.$cmd->output();
-echo "Command complete!\n\n";
-
-$cmd = new \Cmdutils\Cmd(true);
-$cmd->exec('php procs/proc.php');
-echo 'pid: '.$cmd->get_pid()."\n";
-while(true){
-	if($err = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDERR)){
-		echo "ERROR: $err\n";
-	}
-	
-	if($out = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDOUT)){
-		echo "Output: $out\n";
-	}
-	
-	if(!$cmd->is_running()){
-		$exitcode = $cmd->get_exitcode();
-		
-		if($cmd->is_success()){
-			echo "Command successful! ($exitcode)\n";
-		}
-		else{
-			echo "Command failed! ($exitcode)\n";
-		}
-		
-		if($cmd->is_terminated()){
-			echo "Command terminated!\n";
-		}
-		
-		break;
-	}
+try{
+	$Test = new \Cmdutils\Test\Unit;
+	$Test->proc('echo this-test', '', 'this-test');
+	$Test->proc_stream('php procs/proc.php', '', '');
+	$Test->proc_stream('php procs/proc_term.php', '', '', 1);
 }
-
-echo "\n";
-
-$cmd = new \Cmdutils\Cmd(true);
-$cmd->exec('php procs/proc_term.php');
-echo 'pid: '.$cmd->get_pid()."\n";
-while(true){
-	if($err = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDERR)){
-		echo "ERROR: $err\n";
-	}
-	
-	if($out = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDOUT)){
-		echo "Output: $out\n";
-	}
-	
-	if(!$cmd->is_running()){
-		$exitcode = $cmd->get_exitcode();
-		
-		if($cmd->is_success()){
-			echo "Command successful! ($exitcode)\n";
-		}
-		else{
-			echo "Command failed! ($exitcode)\n";
-		}
-		
-		if($cmd->is_terminated()){
-			echo "Command terminated!\n";
-		}
-		
-		break;
-	}
+catch(\Cmdutils\Test\Error $e){
+	echo "Test failed on command: ".$e->getMessage()."\n";
 }
---
+++ test/unit_test/Test.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Cmdutils\Test;
+
+class Unit {
+	public function proc(string $command, string $error, string $output){
+		echo "Test command: '$command'\n---\n";
+		
+		$cmd = new \Cmdutils\Cmd;
+		$err = $cmd->exec($command);
+		
+		echo "ERROR: $err\n";
+		
+		if($error != trim($err)){
+			throw new Error($command);
+		}
+		
+		$out = trim($cmd->output());
+		
+		echo "Output: $out\n";
+		
+		if($output != trim($out)){
+			throw new Error($command);
+		}
+		
+		echo "Command completed!\n\n";
+	}
+	
+	public function proc_stream(string $command, string $error, string $output, int $code=0){
+		echo "Test command: '$command'\n---\n";
+		
+		$cmd = new \Cmdutils\Cmd(true);
+		$cmd->exec($command);
+		
+		echo 'pid: '.$cmd->get_pid()."\n";
+		
+		$test_err = '';
+		$test_out = '';
+		$exitcode = null;
+		
+		while(true){
+			if($err = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDERR)){
+				echo "ERROR: $err\n";
+				
+				$test_err .= "$err\n";
+			}
+			
+			if($out = $cmd->get_pipe_stream(\Cmdutils\Cmd::PIPE_STDOUT)){
+				echo "Output: $out\n";
+				
+				$test_out .= "$out\n";
+			}
+			
+			if(!$cmd->is_running()){
+				$exitcode = $cmd->get_exitcode();
+				
+				if($cmd->is_success()){
+					echo "Command successful! ($exitcode)\n";
+				}
+				else{
+					echo "Command failed! ($exitcode)\n";
+				}
+				
+				break;
+			}
+		}
+		
+		echo "\n";
+	}
+}
+
+class Error extends \Error {}

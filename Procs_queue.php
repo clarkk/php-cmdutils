4abb7800b341aed27d0c47c19f40348162c64586
clarkk <clark@electrobeat.dk>
2020-11-26 02:24:38 +0100

    update

--- Procs_queue.php
+++ Procs_queue.php
@@ -7,26 +7,58 @@ require_once 'Cmd.php';
 use \Utils\Cmd\Cmd;
 
 class Procs_queue {
-	private $procs = [];
+	private $procs 		= [];
 	private $nproc;
 	
-	public function __construct(){
-		$this->nproc = (int)shell_exec('nproc');
+	private $verbose 	= false;
+	
+	const COLOR_GRAY 	= '1;30';
+	const COLOR_GREEN 	= '0;32';
+	const COLOR_YELLOW 	= '1;33';
+	const COLOR_RED 	= '0;31';
+	const COLOR_PURPLE 	= '0;35';
+	
+	const VERBOSE_PLAIN = 1;
+	const VERBOSE_COLOR = 2;
+	
+	public function __construct(int $verbose=0){
+		$this->nproc 	= (int)shell_exec('nproc');
+		$this->verbose 	= $verbose;
+		
 		$this->nproc++;
 	}
 	
 	public function put(string $command){
+		if($this->free_proc_slots()){
+			$proc = new Cmd(true);
+			$proc->exec($command);
+			
+			$pid = $proc->get_pid();
+			
+			if($this->verbose){
+				$this->verbose("Process start (pid: $pid)", self::COLOR_GREEN);
+			}
+		}
+		
 		$this->get_streams();
-		
-		$proc = new Cmd(true);
-		$proc->exec($command);
-		
+	}
+	
+	private function free_proc_slots(): bool{
+		return count($this->procs) < $this->nproc;
 	}
 	
 	private function get_streams(){
-		foreach($this->procs as $p => $proc){
+		foreach($this->procs as $pid => $proc){
 			
 		}
+	}
+	
+	private function verbose(string $string, string $color){
+		if($this->verbose == self::VERBOSE_COLOR){
+			$string = "\033[".$color.'m'.$string."\033[0m";
+		}
+		
+		echo "$string\n";
 	}
 }
 
--- procs_queue/test/test.php
+++ procs_queue/test/test.php
@@ -3,7 +3,7 @@
 require_once '../../Procs_queue.php';
 
 try{
-	$Queue = new \Utils\Procs_queue\Procs_queue;
+	$Queue = new \Utils\Procs_queue\Procs_queue(\Utils\Procs_queue\Procs_queue::VERBOSE_COLOR);
 	
 	$list = [
 		'php procs/proc.php',
@@ -11,21 +11,11 @@ try{
 	];
 	
 	while($list){
-		$command = end($list);
+		$command = reset($list);
 		
 		$Queue->put($command);
 		break;
 	}
-	
-	
-	
-	//$Net = new \Utils\Net\Net;
-	//$Net->request('https://www.google.com');
-	
-	/*$Test = new \Utils\Cmd\Test\Unit;
-	$Test->proc('echo this-test', '', 'this-test');
-	$Test->proc_stream('php procs/proc.php', 'An error occurred!', "First message\nSecond message\nThird message\nFinal message");
-	$Test->proc_stream('php procs/proc_exit.php', 'An fatal error occurred!', 'First message', 1);*/
 }
 catch(\Utils\Procs_queue\Error $e){
 	echo "\n\tTEST FAILED ON URL: ".$e->getMessage()."\n\n";

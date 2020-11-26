ace0480db9e790120454835d7a85d3232bda29a3
clarkk <clark@electrobeat.dk>
2020-11-26 23:42:58 +0100

    update

--- Procs_queue.php
+++ Procs_queue.php
@@ -6,11 +6,17 @@ require_once 'Cmd.php';
 
 use \Utils\Cmd\Cmd;
 
-class Procs_queue {
+abstract class Procs_queue {
+	protected $timeout = 9;
+	
 	private $procs 		= [];
 	private $nproc;
 	
+	private $workers 	= [];
+	
 	private $verbose 	= false;
+	
+	private $time_start;
 	
 	const COLOR_GRAY 	= '1;30';
 	const COLOR_GREEN 	= '0;32';
@@ -21,16 +27,68 @@ class Procs_queue {
 	const VERBOSE_PLAIN = 1;
 	const VERBOSE_COLOR = 2;
 	
-	const TIMEOUT 		= 999;
-	
 	public function __construct(int $verbose=0){
 		$this->nproc 	= (int)shell_exec('nproc');
 		$this->verbose 	= $verbose;
 		
+		// test
 		$this->nproc++;
 	}
 	
-	public function put(string $command){
+	public function add_worker(string $user, string $host, string $tmp_dir){
+		/*$ssh_login = $this->ssh_login($user, $host);
+		
+		$cmd = new Cmd;
+		if($err = $cmd->exec("$ssh_login 'nproc'")){
+			$this->verbose("Worker '$host' failed", self::COLOR_RED);
+		}
+		else{
+			$nproc = (int)$cmd->output();
+			
+			$this->verbose("Worker '$host' initiated width $nproc procs", self::COLOR_GREEN);
+			
+			$this->workers[$host] = [
+				'ssh_login'	=> $ssh_login,
+				'nproc'		=> $nproc
+			];
+			
+			// sh -c 'echo $$; echo $PPID; sleep 10; nproc'
+		}*/
+	}
+	
+	/*$t = microtime(true);
+$session = ssh2_connect('worker.dynaccount.com');
+ssh2_auth_pubkey_file($session, 'root', '/var/www/.ssh/id_rsa.pub', '/var/www/.ssh/id_rsa');
+
+$sftp = ssh2_sftp($session);
+stream_copy_to_stream(fopen("/root/test.pdf", 'r'), fopen("ssh2.sftp://$sftp/root/test.pdf", 'w'));
+
+$stream = ssh2_exec($session, 'nproc');
+$pipe_stdout = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+$pipe_stderr = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
+stream_set_blocking($pipe_stdout, true);
+stream_set_blocking($pipe_stderr, true);
+$stdout = stream_get_contents($pipe_stdout);
+$stderr = stream_get_contents($pipe_stderr);
+echo "stderr: '$stderr'\n";
+echo "stdout: '$stdout'\n";
+echo (microtime(true) - $t)."\n";*/
+	
+	public function exec(){
+		$this->start_time();
+		
+		while(true){
+			if($this->check_timeout()){
+				break;
+			}
+			
+			
+		}
+	}
+	
+	abstract protected function fetch();
+	
+	/*public function put(string $command){
 		if($this->free_proc_slots()){
 			$proc = new Cmd(true);
 			$proc->exec($command);
@@ -43,16 +101,32 @@ class Procs_queue {
 		}
 		
 		$this->get_streams();
-	}
+	}*/
 	
-	private function free_proc_slots(): bool{
-		return count($this->procs) < $this->nproc;
-	}
-	
-	private function get_streams(){
+	/*private function get_streams(){
 		foreach($this->procs as $pid => $proc){
 			
 		}
+	}*/
+	
+	/*private function free_proc_slots(): bool{
+		return count($this->procs) < $this->nproc;
+	}*/
+	
+	private function check_timeout(): bool{
+		return !$this->procs && $this->get_remain_time() >= 0;
+	}
+	
+	private function get_remain_time(): int{
+		return time() - $this->time_start - $this->timeout;
+	}
+	
+	private function start_time(){
+		if($this->verbose){
+			$this->verbose('Starting master process', self::COLOR_GRAY);
+		}
+		
+		$this->time_start = time();
 	}
 	
 	private function verbose(string $string, string $color){
--- procs_queue/test/test.php
+++ procs_queue/test/test.php
@@ -3,25 +3,27 @@
 require_once '../../Procs_queue.php';
 
 class Test_queue extends \Utils\Procs_queue\Procs_queue {
-	
+	protected function fetch(){
+		$list = [
+			'php procs/proc.php',
+			'php procs/proc.php'
+		];
+	}
 }
 
 // tmp folder taskname_2020-11-26-0932_id
 
 try{
 	$Queue = new Test_queue(Test_queue::VERBOSE_COLOR);
+	$Queue->add_worker('root', 'worker.dynaccount.com', '/root/tmp');
+	$Queue->exec();
 	
-	$list = [
-		'php procs/proc.php',
-		'php procs/proc.php'
-	];
-	
-	while($list){
+	/*while($list){
 		$command = reset($list);
 		
 		$Queue->put($command);
 		break;
-	}
+	}*/
 }
 catch(\Utils\Procs_queue\Error $e){
 	echo "\n\tTEST FAILED ON URL: ".$e->getMessage()."\n\n";

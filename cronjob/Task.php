03e1b4e2db28b46bca8c3b468c24993232d344fc
clarkk <clark@electrobeat.dk>
2021-01-02 22:59:30 +0100

    added common methods to task

--- cronjob/Task.php
+++ cronjob/Task.php
@@ -6,10 +6,18 @@ abstract class Task {
 	protected $task_name;
 	protected $verbose = false;
 	
+	private $time_start;
+	
 	public function __construct(string $task_name, int $verbose){
 		$this->task_name 	= $task_name;
 		$this->verbose 		= $verbose;
 		
+		$this->time_start 	= time();
+		
 		$this->exec();
+	}
+	
+	protected function get_remain_time(): int{
+		return time() - $this->time_start - static::TIMEOUT;
 	}
 }

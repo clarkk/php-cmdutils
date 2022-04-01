a7712c4b05f51e9ce0a3ed94becbd51838a32604
clarkk <clark@electrobeat.dk>
2022-04-01 17:28:27 +0200

    improved cmd

--- cmd/Cmd.php
+++ cmd/Cmd.php
@@ -26,19 +26,24 @@ class Cmd extends Proc {
 	}
 	
 	public function output(bool $trim=false, bool $stream_wait=false): string{
-		//	Blocking call: Return stdout
-		if(!$this->is_stream){
-			return $trim ? trim($this->output) : $this->output;
-		}
-		
-		//	Non-blocking call: Wait until stdout returned data
-		if($stream_wait){
-			while(true){
-				if($output = stream_get_contents($this->pipes[self::PIPE_STDOUT])){
-					return $trim ? trim($output) : $output;
+		//	Non-blocking call
+		if($this->is_stream){
+			//	Wait until stdout returns data
+			if($stream_wait){
+				while(true){
+					if($output = stream_get_contents($this->pipes[self::PIPE_STDOUT])){
+						return $trim ? trim($output) : $output;
+					}
 				}
 			}
+			
+			$output = stream_get_contents($this->pipes[self::PIPE_STDOUT]);
+			
+			return $trim ? trim($output) : $output;
 		}
+		
+		//	Blocking call: Return stdout
+		return $trim ? trim($this->output) : $this->output;
 	}
 	
 	public function input(string $data){

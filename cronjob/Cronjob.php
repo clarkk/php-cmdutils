60360d1c197c7a792d8ad7ca64a78024f16909e2
clarkk <clark@electrobeat.dk>
2022-04-14 14:49:35 +0200

    improved cronjob error handling

--- cronjob/Cronjob.php
+++ cronjob/Cronjob.php
@@ -3,7 +3,7 @@
 namespace Utils\Cronjob;
 
 class Cronjob extends Argv {
-	protected $require_task_name = true;
+	protected $require_task_name 	= true;
 	protected $allowed_argv = [
 		self::ARG_V,
 		self::ARG_PROCESS
@@ -14,6 +14,8 @@ class Cronjob extends Argv {
 	
 	private const FAILOVER_TIMEOUT 	= 60;
 	private const FAILOVER_SLEEP 	= 5;
+	
+	private const DB_TABLE 			= 'cronjob';
 	
 	public function init(string $base_path, bool $use_db=true){
 		if(!$this->task_name){
@@ -26,82 +28,95 @@ class Cronjob extends Argv {
 			throw new Error('Cronjob file invalid: '.$this->cronjob_file);
 		}
 		
-		if($use_db){
-			try{
-				$failover_start = time();
-				while(true){
-					//	Start transaction with read lock to prevent multiple of the same cronjob to run in parallel
-					\dbdata\DB::begin();
-					
-					$row = (new \dbdata\Get)
-						->get_lock()
-						->exec('cronjob', [
-							'select' => [
-								'id',
-								'is_running_time'
-							],
-							'where' => [
-								'name' => $this->task_name
-							]
-					])->fetch();
-					
-					//	Return error if cronjob is invalid
-					if(!$row){
-						throw new Error('Cronjob invalid: '.$this->task_name);
-					}
-					
-					if(!$row['is_running_time']){
-						$this->cronjob_id = $row['id'];
-						
-						$this->exec($use_db, $failover_start);
-						
-						break;
-					}
-					else{
-						//	Commit transaction and release read lock
-						\dbdata\DB::commit();
-						
-						if(time() - $failover_start >= self::FAILOVER_TIMEOUT){
-							if($this->verbose){
-								echo "Retry timeout\n";
-							}
-							
-							break;
-						}
-						
-						if($this->verbose){
-							echo "Cronjob '$this->task_name' is already running and has been running for ".(time() - $row['is_running_time'])." secs! Retry in ".self::FAILOVER_SLEEP." secs...\n";
-						}
-						
-						sleep(self::FAILOVER_SLEEP);
-					}
-				}
+		try{
+			if($use_db){
+				$this->try_db();
 			}
-			catch(\dbdata\Error_db $e){
-				$code = $e->getCode();
-				
-				if($this->verbose){
-					echo 'MYSQL error: '.$code.'; '.\Log\Err::format($e)."\n";
-				}
-				
-				\dbdata\DB::rollback();
-				
-				//	MySQL server has gone away
-				if($code == 2006){
-					
-				}
-				else{
-					\Log\Err::catch_all($e);
-				}
-			}
-			catch(\Throwable $e){
-				\dbdata\DB::rollback();
-				
-				\Log\Err::catch_all($e);
+			else{
+				$this->exec(false);
 			}
 		}
-		else{
-			$this->exec($use_db);
+		//	Catch DB errors
+		catch(\dbdata\Error_db $e){
+			$code 	= $e->getCode();
+			$error 	= 'MYSQL error: '.$code.'; '.\Log\Err::format($e);
+			
+			\dbdata\DB::rollback();
+			
+			switch($code){
+				//	MySQL server has gone away
+				case 2006:
+					\Log\Log::err(\Log\Log::ERR_FATAL, $error);
+					
+					echo "$error\n";
+					break;
+				
+				default:
+					\Log\Err::catch_all($e);
+			}
+		}
+		//	Catch fatal errors
+		catch(\Throwable $e){
+			if($use_db){
+				\dbdata\DB::rollback();
+			}
+			
+			$error = \Log\Err::format($e);
+			\Log\Log::err(self::DB_TABLE, $this->task_name.': '.$error, false);
+			
+			echo "$error\n";
+		}
+	}
+	
+	private function try_db(){
+		$failover_start = time();
+		
+		while(true){
+			//	Start transaction with read lock to prevent multiple of the same cronjob to run in parallel
+			\dbdata\DB::begin();
+			
+			//	Return error if cronjob is invalid
+			if(!$row = (new \dbdata\Get)
+				->get_lock()
+				->exec(self::DB_TABLE, [
+					'select' => [
+						'id',
+						'is_running_time'
+					],
+					'where' => [
+						'name' => $this->task_name
+					]
+			])->fetch()){
+				throw new Error('Cronjob invalid: '.$this->task_name);
+			}
+			
+			//	Execute cronjob
+			if(!$row['is_running_time']){
+				$this->cronjob_id = $row['id'];
+				
+				$this->exec(true, $failover_start);
+				
+				break;
+			}
+			//	Cronjob is already executing
+			else{
+				//	Commit transaction and release read lock on cronjob
+				\dbdata\DB::commit();
+				
+				if(time() - $failover_start >= self::FAILOVER_TIMEOUT){
+					if($this->verbose){
+						echo "Failover timeout\n";
+					}
+					
+					break;
+				}
+				
+				if($this->verbose){
+					echo "Cronjob '$this->task_name' is already running and has been running for ".(time() - $row['is_running_time'])." secs! Failover sleep ".self::FAILOVER_SLEEP." secs...\n";
+				}
+				
+				sleep(self::FAILOVER_SLEEP);
+			}
 		}
 	}
 	
@@ -112,20 +127,23 @@ class Cronjob extends Argv {
 			echo "Cronjob '$this->task_name' starts executing (pid: $pid)...\n";
 		}
 		
-		$time = time();
+		$time_start = time();
 		
 		if($use_db){
 			$this->update_cronjob([
-				'is_running_time'		=> $time,
+				'is_running_time'		=> $time_start,
 				'is_failure_notified'	=> 0,
-				'time'					=> $time,
-				'time_offset'			=> $time - $failover_start,
+				'time'					=> $time_start,
+				'time_offset'			=> $time_start - $failover_start,
 				'ppid'					=> posix_getppid(),
 				'pid'					=> $pid
 			]);
 			
-			//	Commit transaction and release read lock
+			//	Commit transaction and release read lock on cronjob
 			\dbdata\DB::commit();
+			
+			//	Start transaction for the task
+			\dbdata\DB::begin();
 		}
 		
 		require_once $this->cronjob_file;
@@ -135,21 +153,14 @@ class Cronjob extends Argv {
 			throw new Error('Cronjob class missing: '.$class_name);
 		}
 		
-		//	Start transaction for the task
-		if($use_db){
-			\dbdata\DB::begin();
-		}
-		
 		new $class_name($this->task_name, $this->verbose);
 		
-		//	Commit transaction for the task
+		$time_exec = time() - $time_start;
+		
 		if($use_db){
+			//	Commit transaction for the task
 			\dbdata\DB::commit();
-		}
-		
-		$time_exec = time() - $time;
-		
-		if($use_db){
+			
 			$this->update_cronjob([
 				'is_running_time'	=> 0,
 				'time_offset'		=> 0,
@@ -160,9 +171,12 @@ class Cronjob extends Argv {
 			
 			(new \dbdata\Put)->exec('log_cronjob', 0, [
 				'cronjob_id'	=> $this->cronjob_id,
-				'time'			=> $time,
+				'time'			=> $time_start,
 				'time_exec'		=> $time_exec
 			]);
+		}
+		else{
+			\Log\Log::log(self::DB_TABLE, $this->task_name.' started '.\Time\Time::timestamp($time_start, true).' ('.$time_exec.' secs)');
 		}
 		
 		if($this->verbose){
@@ -171,7 +185,7 @@ class Cronjob extends Argv {
 	}
 	
 	private function update_cronjob(array $update){
-		(new \dbdata\Put)->exec('cronjob', $this->cronjob_id, $update);
+		(new \dbdata\Put)->exec(self::DB_TABLE, $this->cronjob_id, $update);
 	}
 }
 

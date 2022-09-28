ded07d46a54dc848531076020d4c5c47f53d35b6
clarkk <clark@electrobeat.dk>
2022-09-29 00:01:33 +0200

    added cache

---
+++ cache/Cache.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Utils\Cache;
+
+class Cache {
+	private \Redis $redis;
+	
+	public function __construct(string $auth){
+		$this->redis = new \Redis;
+		$this->redis->connect('127.0.0.1');
+		$this->redis->auth($auth);
+	}
+	
+	public function write($buffer): void{
+		$buffer->send($this->redis);
+	}
+	
+	public function close(): void{
+		$this->redis->close();
+	}
+}

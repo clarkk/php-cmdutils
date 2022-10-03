02f0755a9120955e371c8ce2d7de0ada23f98591
clarkk <clark@electrobeat.dk>
2022-10-03 22:21:58 +0200

    added string cache

--- cache/Cache.php
+++ cache/Cache.php
@@ -19,12 +19,20 @@ class Cache {
 		return $this->redis;
 	}
 	
-	public function write_json(string $key, array $data, int $expire){
-		$this->redis->setEx($key, $expire, self::json_encode($data));
+	public function write(string $key, string $data, int $expire): void{
+		$this->redis->setEx($key, $expire, $data);
+	}
+	
+	public function write_json(string $key, array $data, int $expire): void{
+		$this->write($key, self::json_encode($data), $expire);
+	}
+	
+	public function fetch(string $key): string{
+		return $this->redis->get($key);
 	}
 	
 	public function fetch_json(string $key): array{
-		return json_decode($this->redis->get($key), true) ?? [];
+		return json_decode($this->fetch($key), true) ?? [];
 	}
 	
 	public function close(): void{
